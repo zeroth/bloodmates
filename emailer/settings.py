255183f38aaeefe4f9cc5d8b901b3164d4bddc6e
Abhishek Patil <abhishek@thezeroth.net>
2013-11-24 00:05:32 +0100

    settings change
    Signed-off-by: Abhishek Patil <abhishek@thezeroth.net>

--- emailer/settings.py
+++ emailer/settings.py
@@ -149,6 +149,8 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'mail',
+    'mainsite',
 )
 
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
